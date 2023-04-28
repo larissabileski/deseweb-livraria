295b7c8fb5df5e808e0fb66673162b971425c7c7
larissabileski <laricrisinabileski@gmail.com>
2023-04-28 14:33:31 -0300

    LivroListSerializer criado

--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Categoria, Editora, Autor, Livro
-from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -20,8 +20,9 @@ class AutorViewSet(ModelViewSet):
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-
     def get_serializer_class(self):
-        if self.action in ["list", "retrieve"]:
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
             return LivroDetailSerializer
         return LivroSerializer
