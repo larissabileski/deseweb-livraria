b7f6aa9a68a9864dc36770fcabfc5064e8a9e4f2
larissabileski <laricristinabileski@gmail.com>
2023-06-27 14:39:26 -0300

    Serializer autor criada

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,18 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria, Editora, Autor, Livro
-
-class CategoriaSerializer(ModelSerializer):
-    class Meta:
-        model = Categoria
-        fields = "__all__"
-
-
-class EditoraSerializer(ModelSerializer):
-    class Meta:
-        model = Editora
-        fields = "__all__"
-
+from livraria.models import Autor, Livro
 
 class AutorSerializer(ModelSerializer):
     class Meta:
