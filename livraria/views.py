0388470bfdfa08e2516cfc32e96e54119ac2f4ba
larissabileski <laricristinabileski@gmail.com>
2023-06-27 14:28:05 -0300

    View categoria criada

--- livraria/views.py
+++ livraria/views.py
@@ -1,12 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora, Autor, Livro
-from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
-
-class CategoriaViewSet(ModelViewSet):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
-
+from livraria.models import Editora, Autor, Livro
+from livraria.serializers import EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
