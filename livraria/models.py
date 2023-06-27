da1d468ffa72d370c0a46871204bd246465228b6
larissabileski <laricristinabileski@gmail.com>
2023-06-27 14:17:49 -0300

    Model autor criada

--- livraria/models.py
+++ livraria/models.py
@@ -14,18 +14,6 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
-    
-
-class Autor(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
-    
-    class Meta:
-        verbose_name_plural = "Autores"
-
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
