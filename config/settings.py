3c812df43a798941e2f6068b55d258823cfb3e80
larissabileski <laricristinabileski@gmail.com>
2023-07-25 14:39:50 -0300

    Modificando o usuário padrão do Django

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    "rest_framework_simplejwt",
+    'rest_framework_simplejwt',
+    'usuario',
     'livraria',
 ]
 
@@ -126,6 +127,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTH_USER_MODEL = "usuario.Usuario"
 
 # REST_FRAMEWORK = {
 #     "DEFAULT_PERMISSION_CLASSES": [
