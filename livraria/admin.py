a8b288c903a1aca573be9e665468f193b67e14ec
larissabileski <laricristinabileski@gmail.com>
2023-08-11 14:52:19 -0300

    Uso de TabularInline no Admin para Itens da Compra

--- livraria/admin.py
+++ livraria/admin.py
@@ -6,6 +6,12 @@ admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
-admin.site.register(Compra)
-admin.site.register(ItensCompra)
+# admin.site.register(Compra)
+# admin.site.register(ItensCompra)
 
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
