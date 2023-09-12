2532ecaef37db77feac6418e9ff46ca83e886a02
larissabileski <laricristinabileski@gmail.com>
2023-09-12 13:28:55 -0300

    Mostrando + detalhes do livro na listagem

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -5,8 +5,10 @@ from livraria.models import Compra, ItensCompra
 class ItensCompraSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = "__all__"
+        fields = ["livro", "quantidade"]
+        depth = 2
 
+        
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
